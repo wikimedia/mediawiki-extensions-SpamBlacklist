31e30af1b21531f38509122c666d06a99f171a27
Tim Starling <tstarling@users.mediawiki.org>
2005-06-25 15:49:21 +0000

    Support for HTTP, including working default, to load text from meta once per hour. Special attention paid to reducing load on meta, of course.

--- SpamBlacklist_body.php
+++ SpamBlacklist_body.php
@@ -6,13 +6,16 @@ class SpamBlacklist {
 	var $regex = false;
 	var $previousFilter = false;
 	var $files = array();
+	var $warningTime = 600;
+	var $expiryTime = 900;
+	var $warningChance = 100;
 	
 	function SpamBlacklist() {
 		global $IP;
-		$this->files = array( "$IP/extensions/SpamBlacklist/wikimedia_blacklist" );
+		$this->files = array( "http://meta.wikimedia.org/wiki/Spam_blacklist?action=raw&sb_ver=1" );
 	}
 	function filter( &$title, $text, $section ) {
-		global $wgArticle, $wgDBname, $wgMemc, $wgOut;
+		global $wgArticle, $wgDBname, $wgMemc, $messageMemc, $wgVersion, $wgOut;
 
 		$fname = 'wfSpamBlacklistFilter';
 		wfProfileIn( $fname );
@@ -54,6 +57,7 @@ class SpamBlacklist {
 			if ( !$this->regex ) {
 				# Load lists
 				$lines = array();
+				wfDebug( "Constructing spam blacklist\n" );
 				foreach ( $this->files as $fileName ) {
 					if ( preg_match( '/^DB: (\w*) (.*)$/', $fileName, $matches ) ) {
 						if ( $wgDBname == $matches[1] && $title->getPrefixedDBkey() == $matches[2] ) {
@@ -61,6 +65,30 @@ class SpamBlacklist {
 						} else {
 							$lines += $this->getArticleLines( $matches[1], $matches[2] );
 						}
+					} elseif ( preg_match( '/^http:\/\//', $fileName ) ) {
+						# HTTP request
+						# To keep requests to a minimum, we save results into $messageMemc, which is
+						# similar to $wgMemc except almost certain to exist. By default, it is stored
+						# in the database
+						#
+						# There are two keys, when the warning key expires, a random thread will refresh
+						# the real key. This reduces the chance of multiple requests under high traffic 
+						# conditions.
+						$key = "spam_blacklist_file:$fileName";
+						$warningKey = "$wgDBname:spamfilewarning:$fileName";
+						$httpText = $messageMemc->get( $key );
+						$warning = $messageMemc->get( $warningKey );
+
+						if ( !is_string( $httpText ) || ( !$warning && !mt_rand( 0, $this->warningChance ) ) ) {
+							wfDebug( "Loading spam blacklist from $fileName\n" );
+							$url_fopen = ini_set( 'allow_url_fopen', 1 );
+							$httpText = file_get_contents( $fileName );
+							ini_set( 'allow_url_fopen', $url_fopen );
+							$messageMemc->set( $warningKey, 1, $this->warningTime );
+							$messageMemc->set( $key, $httpText, $this->expiryTime );
+						}
+						
+						$lines += explode( "\n", $httpText );
 					} else {
 						$lines += file( $fileName );
 					}
@@ -71,6 +99,7 @@ class SpamBlacklist {
 
 				# No lines, don't make a regex which will match everything
 				if ( count( $lines ) == 0 ) {
+					wfDebug( "No lines\n" );
 					$this->regex = true;
 				} else {
 					# Make regex
@@ -83,7 +112,9 @@ class SpamBlacklist {
 		}
 		if ( $this->regex !== true ) {
 			# Do the match
+			wfDebug( "Checking text against regex: {$this->regex}\n" );
 			if ( preg_match( $this->regex, $text, $matches ) ) {
+				wfDebug( "Match!\n" );
 				EditPage::spamPage( $matches[0] );
 				$retVal = true;
 			} else {
