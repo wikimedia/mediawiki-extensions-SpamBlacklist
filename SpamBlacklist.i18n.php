6af93e13c55d995091c9757c4f0ad139793c3cbf
Raimond Spekking <raymond@users.mediawiki.org>
2007-07-24 11:18:26 +0000

    Updates German

--- SpamBlacklist.i18n.php
+++ SpamBlacklist.i18n.php
@@ -41,6 +41,36 @@ function efSpamBlacklistMessages() {
 	" and {{PLURAL:$1|needs|need}} to be corrected before saving the page:\n",
 ),
 
+'de' => array(
+	'spam-blacklist' => '
+ # Externe URLs, die in dieser Liste enthalten sind, blockieren das Speichern der Seite.
+ # Diese Liste betrifft nur dieses Wiki; siehe auch die globale Blacklist.
+ # Zur Dokumenation siehe http://www.mediawiki.org/wiki/Extension:SpamBlacklist
+ #<!-- Diese Zeile darf nicht verändert werden! --> <pre>
+#
+# Syntax:
+#   * Alles ab dem "#"-Zeichen bis zum Ende der Zeile ist ein Kommentar
+#   * Jede nicht-leere Zeile ist ein regulärer Ausdruck, der gegen die Host-Namen in den URLs geprüft wird.
+
+ #</pre> <!-- Diese Zeile darf nicht verändert werden! -->',
+
+	'spam-whitelist' => '
+ #<!-- Diese Zeile darf nicht verändert werden! --> <pre>
+# Externe URLs, die in dieser Liste enthalten sind, blockieren das Speichern der Seite nicht, auch wenn sie
+# in der globalen oder lokalen schwarzen Liste enthalten sind.
+#
+# Syntax:
+#   * Alles ab dem "#"-Zeichen bis zum Ende der Zeile ist ein Kommentar
+#   * Jede nicht-leere Zeile ist ein regulärer Ausdruck, der gegen die Host-Namen in den URLs geprüft wird.
+
+ #</pre> <!-- Diese Zeile darf nicht verändert werden! -->',
+
+	'spam-invalid-lines' =>
+	"{{PLURAL:$1
+	| Die folgende Zeile in der Spam-Blacklist ist ein ungültiger regulärer Ausdruck. Sie muss vor dem Speichern der Seite korrigiert werden
+	| Die folgenden Zeilen in der Spam-Blacklist sind ungültige reguläre Ausdrücke. Sie müssen vor dem Speichern der Seite korrigiert werden}}:\n",
+),
+
 'id' => array(
 	'spam-blacklist' => '
  # Pranala luar yang cocok dengan daftar ini akan diblok ketika ditambahkan ke suatu halaman.
