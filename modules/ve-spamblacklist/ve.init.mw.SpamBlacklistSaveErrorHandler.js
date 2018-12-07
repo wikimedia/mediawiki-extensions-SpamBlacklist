a4229f339e9675dfadeee958f93695d08ab621a6
Bartosz Dziewoński <matma.rex@gmail.com>
2018-12-07 14:17:55 -0500

    ve.init.mw.SpamBlacklistSaveErrorHandler: Update to receive entire response
    Change-Id: Ifadf23d572a77e082edaa80e6c97792f8c1af4d3
    Depends-On: I818d916275b8451af6910ddaa7cd4d7c653085ee

--- modules/ve-spamblacklist/ve.init.mw.SpamBlacklistSaveErrorHandler.js
+++ modules/ve-spamblacklist/ve.init.mw.SpamBlacklistSaveErrorHandler.js
@@ -6,14 +6,14 @@ mw.libs.ve.targetLoader.addPlugin( function () {
 
 	ve.init.mw.SpamBlacklistSaveErrorHandler.static.name = 'spamBlacklist';
 
-	ve.init.mw.SpamBlacklistSaveErrorHandler.static.matchFunction = function ( editApi ) {
-		return !!editApi.spamblacklist;
+	ve.init.mw.SpamBlacklistSaveErrorHandler.static.matchFunction = function ( data ) {
+		return !!ve.getProp( data, 'visualeditoredit', 'edit', 'spamblacklist' );
 	};
 
-	ve.init.mw.SpamBlacklistSaveErrorHandler.static.process = function ( editApi, target ) {
+	ve.init.mw.SpamBlacklistSaveErrorHandler.static.process = function ( data, target ) {
 		// Handle spam blacklist error from Extension:SpamBlacklist
 		target.showSaveError(
-			$( $.parseHTML( editApi.sberrorparsed ) ),
+			$( $.parseHTML( ve.getProp( data, 'visualeditoredit', 'edit', 'sberrorparsed' ) ) ),
 			false // prevents reapply
 		);
 		// Emit event for tracking. TODO: This is a bad design
